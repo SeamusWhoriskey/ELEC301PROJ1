da51d8fb9e54e426bc89e9dc0f9b2957740461b6
seamus <whoriskey1@gmail.com>
2020-10-27 22:11:54 -0400

    updated + cleaned up (a bit)

--- dct.py
+++ dct.py
@@ -2,12 +2,9 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import matplotlib.image as im
-import visualizations as vz
 import copy
 
 pi = math.pi
-
-# img = im.imread('test_images/richb8.png')
 
 img = im.imread('grey_noised_imgs/cameraman_original.png')
 noisy_img = im.imread('grey_noised_imgs/cameraman_025.png')
@@ -22,6 +19,10 @@ def create_onb_DCT(N):
         d[:, k] = d[:, k]/np.linalg.norm(d[:, k])
 
     return d
+
+
+def calc_mse(im1, im2):
+    return round(np.square(im1-im2).mean(axis=None), 6)
 
 
 def add_noise(image, var):
@@ -46,27 +47,47 @@ def soft_threshold(image, t):
 
 def fDCT(image):
     onb1 = np.linalg.inv(create_onb_DCT(image.shape[0]))
-    onb2 = np.linalg.inv(create_onb_DCT(image.shape[1]))
-    return np.matmul(np.matmul(onb1, image), onb2)
+    onb2 = np.linalg.inv(create_onb_DCT(image.shape[1]).T)
+    return np.matmul(onb1, np.matmul(image, onb2))
+
+
+def fDCT_block(image, onb):
+    return np.matmul(onb, np.matmul(image, onb.T))
 
 
 def iDCT(image):
     onb1 = create_onb_DCT(image.shape[0])
-    onb2 = create_onb_DCT(image.shape[1])
+    onb2 = create_onb_DCT(image.shape[1]).T
     return np.matmul(np.matmul(onb1, image), onb2)
+
+
+def iDCT_block(image, onb):
+    return np.matmul(np.matmul(onb, image), onb.T)
 
 
 def compress_image_DCT_hard(image, thresh):
     out = np.zeros(image.shape)
-    # RBG
+    # RGB
     if len(image.shape) == 3:
         for n in range(image.shape[2]):
             im_c = iDCT(image[:, :, n])
-            out[:, :, n] = hard_threshold(im_c, thresh)
+            image_median_value = np.mean(im_c)
+            deviation = im_c - image_median_value
+            abs_deviation = np.absolute(deviation)
+            abs_deviation_median_value = np.median(abs_deviation)
+            dev = abs_deviation_median_value / 0.6745
+            out[:, :, n] = hard_threshold(im_c, math.sqrt(dev) * est_thresh(image.shape[0] * image.shape[1]))
+
     # B+W
     else:
         im_c = iDCT(image[:, :])
-        out[:, :] = hard_threshold(im_c, thresh)
+        image_median_value = np.median(im_c)
+        deviation = im_c - image_median_value
+        abs_deviation = np.absolute(deviation)
+        abs_deviation_median_value = np.median(abs_deviation)
+        dev = abs_deviation_median_value / 0.6745
+        out[:, :] = hard_threshold(im_c, math.sqrt(dev) * est_thresh(image.shape[0] * image.shape[1]))
+
     return out
 
 
@@ -76,11 +97,22 @@ def compress_image_DCT_soft(image, thresh):
     if len(image.shape) == 3:
         for n in range(image.shape[2]):
             im_c = iDCT(image[:, :, n])
-            out[:, :, n] = soft_threshold(im_c, thresh)
+            image_median_value = np.median(im_c)
+            deviation = im_c - image_median_value
+            abs_deviation = np.absolute(deviation)
+            abs_deviation_median_value = np.median(abs_deviation)
+            dev = abs_deviation_median_value / 0.6745
+            out[:, :, n] = soft_threshold(im_c, math.sqrt(dev) * est_thresh(image.shape[0] * image.shape[1]))
     # B+W
     else:
         im_c = iDCT(image[:, :])
-        out[:, :] = soft_threshold(im_c, thresh)
+        image_median_value = np.median(im_c)
+        deviation = im_c - image_median_value
+        abs_deviation = np.absolute(deviation)
+        abs_deviation_median_value = np.median(abs_deviation)
+        dev = abs_deviation_median_value / 0.6745
+        out[:, :] = soft_threshold(im_c, math.sqrt(dev) * est_thresh(image.shape[0] * image.shape[1]))
+
     return out
 
 
@@ -184,7 +216,6 @@ def do_block_compression_averaged_soft(image, t, blk_sz, step_sz):
         c_img = do_block_compression_soft(image[i:, i:], t, blk_sz)
         im_cpy[i:, i:] = c_img
         tot_img += im_cpy
-        # c_img[0:image.shape[0] - i, 0:image.shape[1] - i]
     return tot_img/blk_sz
 
 
@@ -195,7 +226,6 @@ def do_block_compression_averaged_hard(image, t, blk_sz, step_sz):
         c_img = do_block_compression_hard(image[i:, i:], t, blk_sz)
         im_cpy[i:, i:] = c_img
         tot_img += im_cpy
-        # c_img[0:image.shape[0] - i, 0:image.shape[1] - i]
     return tot_img/blk_sz
 
 
@@ -219,48 +249,48 @@ def show_all_compressions(image, blk_sz):
 
     # soft
     # s = do_compression_soft(noisy_image, t)
-    # hard
+    # # # hard
     # h = do_compression_hard(noisy_image, t)
-    # soft block
+    # # soft block
     # sb = do_block_compression_soft(noisy_image, bt, blk_sz)
-    s = do_block_compression_hard(noisy_image, bt, blk_sz)
-    h = do_block_compression_averaged_hard(noisy_image, bt, blk_sz, 1)
-    sb = do_block_compression_averaged_hard(noisy_image, bt, blk_sz, 2)
-    hb = do_block_compression_averaged_hard(noisy_image, bt, blk_sz, 4)
+    s = do_block_compression_soft(noisy_image, bt, blk_sz)
+    sb = do_block_compression_averaged_soft(noisy_image, bt, blk_sz, 1)
+    hb = do_block_compression_averaged_hard(noisy_image, bt, blk_sz, 1)
+    # hb = do_block_compression_averaged_soft(noisy_image, bt, blk_sz, 4)
     # hard block
-    # hb = do_block_compression_hard(noisy_image, bt, blk_sz)
-    ax1 = fig.add_subplot(321)
-    plt.title("Original image")
-    plt.imshow(image, interpolation='nearest', cmap='gray')
-    ax1.xaxis.set_visible(False)
-    ax1.yaxis.set_visible(False)
-    ax2 = fig.add_subplot(322)
-    plt.title("Image after noise added (var = " + str(var) + ")")
-    plt.imshow(noisy_image, interpolation='nearest', cmap='gray')
-    ax2.xaxis.set_visible(False)
-    ax2.yaxis.set_visible(False)
+    h = do_block_compression_hard(noisy_image, bt, blk_sz)
+    # ax1 = fig.add_subplot(321)
+    # plt.title("Original image")
+    # plt.imshow(image, interpolation='nearest', cmap='gray')
+    # ax1.xaxis.set_visible(False)
+    # ax1.yaxis.set_visible(False)
+    # ax2 = fig.add_subplot(322)
+    # plt.title("Image after noise added (var = " + str(var) + ")")
+    # plt.imshow(noisy_image, interpolation='nearest', cmap='gray')
+    # ax2.xaxis.set_visible(False)
+    # ax2.yaxis.set_visible(False)
 
     ax3 = fig.add_subplot(323)
-    plt.title("Soft threshold")
+    plt.title("Soft  block threshold \n(mse = " + str(calc_mse(image, s)) + ")")
     plt.imshow(s, interpolation='nearest', cmap='gray')
     ax3.xaxis.set_visible(False)
     ax3.yaxis.set_visible(False)
 
     ax4 = fig.add_subplot(324)
-    plt.title("Hard threshold")
+    plt.title("Hard threshold \n(mse = " + str(calc_mse(image, h)) + ")")
     plt.imshow(h, interpolation='nearest', cmap='gray')
     ax4.xaxis.set_visible(False)
     ax4.yaxis.set_visible(False)
 
     ax5 = fig.add_subplot(325)
-    plt.title("block size = " + str(blk_sz), fontsize=6)
+    plt.title("averaged\n(mse = " + str(calc_mse(image, sb)) + ")", fontsize=6)
 
     plt.imshow(sb, interpolation='nearest', cmap='gray')
     ax5.xaxis.set_visible(False)
     ax5.yaxis.set_visible(False)
 
     ax6 = fig.add_subplot(326)
-    plt.title("block size = " + str(blk_sz), fontsize=6)
+    plt.title("(mse = " + str(calc_mse(image, hb)) + ")", fontsize=6)
     plt.imshow(hb, interpolation='nearest', cmap='gray')
     ax6.xaxis.set_visible(False)
     ax6.yaxis.set_visible(False)
